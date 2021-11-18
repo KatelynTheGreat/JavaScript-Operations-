846b25570188b5c737d84e38194d584f48577064
KatelynTheGreat <89535173+KatelynTheGreat@users.noreply.github.com>
2021-11-18 13:02:29 -0500

    Add files via upload

---
+++ Operations/Lab1/lab1.js
@@ -0,0 +1,26 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAnHour = secondsInAMinute * minutesInAHour;
+var secondsInADay = secondsInAnHour * hoursInADay;
+var secondsInAYear = secondsInADay * daysInAYear;
+
+
+document.writeln("The number of seconds in an hour is " + secondsInAnHour);
+document.writeln("<br>");
+
+
+
+
+document.writeln("The number of seconds in a day are " + secondsInADay);
+document.writeln("<br>");
+
+
+
+
+document.writeln("The number of seconds in a year are " + secondsInAYear);
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("Created by Katelyn Hill");
---
+++ Operations/Lab2/lab1_2.js
@@ -0,0 +1,24 @@
+var numberOfApples = 10;
+
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+
+document.writeln("<br><br>");
+
+var numberOfNinja = 100;
+document.writeln("There are " + --numberOfNinja + "ninjas in the room. <br>");
+document.writeln("There are " + --numberOfNinja + "ninjas in the room. <br>");
+document.writeln("There are " + --numberOfNinja + "ninjas in the room. <br>");
+document.writeln("There are " + --numberOfNinja + "ninjas in the room. <br>");
+document.writeln("There are " + --numberOfNinja + "ninjas in the room. <br>");
+document.writeln("Created by Katelyn Hill.")
